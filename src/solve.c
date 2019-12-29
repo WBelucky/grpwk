5536a1785ff8ceb642d52fa8ee01dd693f6dfff3
WBelucky <shintaro.kusabiraki@gmail.com>
2019-12-30 01:24:57 +0900

    add params

--- src/solve.c
+++ src/solve.c
@@ -1,14 +1,16 @@
 #include <stdlib.h>
 #include <string.h>
-#include "bm.h"
 #include <stdio.h>
 
+#include "params.h"
+#include "bm.h"
 // qsortのための比較関数
 int str_len_cmp_r(const void * a, const void * b) {
     return (int)strlen(*(char**)b) - (int)strlen(*(char**)a);
 }
 
-void solve(char * t, char** s, int n) {
+void solve(char * t, char** s, int n, Params* p) {
+  printf("params = %d\n", p->bm_search_limit_length);
 
   // 文字列の長さでソートしたら精度が上がる
   qsort(s, (size_t)n , sizeof(char*), str_len_cmp_r);
@@ -16,7 +18,7 @@ void solve(char * t, char** s, int n) {
   int t_length = (int)strlen(t);
 
   // s[i]をそれぞれ見ていって, tのある部分とマッチしたらそこを書き換え.
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < p->bm_search_limit_length; i++) {
     int s_length = (int)strlen(s[i]);
     char * match = bm_search(t, t_length, s[i], s_length);
     if (match == NULL) {
